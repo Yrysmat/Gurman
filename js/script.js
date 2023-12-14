67a0768d97446899746077145b8a64733aee9ae0
Yrysmat <136898618+Yrysmat@users.noreply.github.com>
2023-12-14 16:50:54 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const headerBtn = document.querySelector('.header__btn');
 const headBtnSun = document.querySelector(".header__btn-sun");
 const headBtnMoon = document.querySelector(".header__btn-moon");
-const headBtnLink = document.querySelector(".header__button")
+const headBtnLink = document.querySelector(".header__button");
+let headBtnWhatsapp = document.querySelector(".header__whatsapp");
+let headBtnTelegram = document.querySelector(".header__telegram")
 
 headerBtn.addEventListener("click", () => {
     document.body.classList.toggle('dark-theme');
@@ -9,3 +11,26 @@ headerBtn.addEventListener("click", () => {
     headBtnMoon.classList.toggle('moon')
     // heroTitle.classList.toggle('dark')
 });
+headBtnLink.addEventListener("click", () =>{
+    headBtnWhatsapp.classList.toggle('active')
+    headBtnTelegram.classList.toggle('active')
+
+});
+
+// headBtnLink.addEventListener("click", () => {
+//     document.body.classList.toggle('.linking')
+//     headerBtnLinkedBlock.classList.toggle('block')
+
+// })
+// $(function() {
+//     let header = $('.header');
+     
+//     $(window).scroll(function() {
+//       if($(this).scrollTop() > 1) {
+//        header.addClass('header_fixed');
+//       } else {
+//        header.removeClass('header_fixed');
+//       }
+//     });
+//    });
+
